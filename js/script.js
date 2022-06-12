e0fdec28df7f630f4901607a088c00c3c408c4c5
morgenshtern9999 <95570339+morgenshtern9999@users.noreply.github.com>
2022-06-12 14:39:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('Оно не работает ладно шутка');
+
+const header = document.querySelector('.siteHeader');
+const spider = document.querySelector('.spider');
+
+document.querySelector('.showOn').onchange =function(){
+    if(this.checked){
+        header.classList.add('siteHeaderBg');
+        spider.classList.add('spiderAnimation');
+    }
+    else{
+        header.classList.remove('siteHeaderBg');
+        spider.classList.remove('spiderAnimation');
+    }
+}
+
+//------------------Sounde
+
+function scarySound(){
+    const audio = new Audio();
+    audio.preload = 'auto';
+    audio.src = '../img/monstr.mp3';
+    audio.play();
+}
+
+spider.onclick = scarySound
